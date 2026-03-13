f664ad53467b985ef252ad8be412b4df0af68515
Lucas Gabriel <lucasgc2510@gmail.com>
2026-03-13 16:54:34 -0300

    bugs fix

--- program/class_and_functions.py
+++ program/class_and_functions.py
@@ -1,6 +1,10 @@
 import random
 def line():
+    """
+    shows a line
+    """
     print('—' * 40)
+
 
 
 def menu():
@@ -14,26 +18,20 @@ def menu():
     print("4 - Finish program")
     line()
 
+def show_logins(self):
+    """
+    show all accounts created by user
+    including the username, password, and account source
+    """
+    pass
 
-    
-class User:
-    def __init__(self, name):
-        # attributes
-        self.name = name
-        self.username = ''
-        self.login_place = ''
-        self.key_access = self.generate_password()
-        self.password = self.generate_password()
-    
-    # methods
-    def generate_data(self):
-        """
-        create a archive where will have data about the user's accounts
-        """
-        with open('data_passwords.txt', 'w') as archive:
-            pass
+def search_password(self):
+    """
+    search an especific password and user, with the account source
+    """
+    pass
 
-    def generate_password(self):
+def generate_password():
         """
         generate a secure password for the user
         """
@@ -41,31 +39,16 @@ class User:
         password = ''.join(random.choice(characters) for i in range(12))
         return password
     
-    def menu(self):
-        """
-        show the main menu with options to user choice
-
-        """
-        line()
-        print("1 - Access your passwords")
-        print("2 - Create new passwords for new accounts")
-        print("3 - Search for an specific password")
-        print("4 - Finish program")
-        line()
-
-    def show_logins(self):
-        """
-        show all accounts created by user
-        including the username, password, and account source
-        """
-        pass
-    def search_password(self):
-        """
-        search an especific password and user, with the account source
-        """
-        pass
+class User:
+    def __init__(self, name, login_place):
+        # attributes
+        self.username = name
+        self.login_place = login_place
+        self.password = generate_password()
+    
+    # methods
     def new_login(self):
         """
         Register a login for the user and create a password for the account
         """
-        pass
+        
